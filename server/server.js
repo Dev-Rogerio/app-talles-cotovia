dc7c91a00ef3b88eb7c8703792b97a3dfb434a5f
Rogerio Almeida <roger.ngt3494@gmail.com>
2025-03-25 13:30:26 -0300

    atualiza CORS para cotovia.netlify.app

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://cotovia.netlify.app/"], // seu frontend publicado
+    origin: ["https://cotovia.netlify.app"], // nova URL do frontend
     methods: ["GET", "POST"],
     credentials: true,
   })
